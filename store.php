ec85f99153761a4de216a4dc525ebbd2dbb8c057
Jahida Tamanna <84682527+jahidatamanna@users.noreply.github.com>
2021-06-28 00:04:29 +0600

    Add files via upload

---
+++ store.php
@@ -0,0 +1,69 @@
+<?php  
+ $message = '';  
+ $error = '';  
+ if(isset($_POST["submit"]))  
+ {  
+      if(empty($_POST["email"]))  
+      {  
+           $error = "<label class='text-danger'>Enter an e-mail</label>";  
+      }  
+       
+      else  
+      {  
+           if(file_exists('data.json'))  
+           {  
+                $current_data = file_get_contents('data.json');  
+                $array_data = json_decode($current_data, true);  
+                $extra = array(
+                     'e-mail'          =>     $_POST["email"]  
+                );  
+                $array_data[] = $extra;  
+                $final_data = json_encode($array_data);  
+                if(file_put_contents('data.json', $final_data))  
+                {  
+                     $message = "<label class='text-success'>FILE EXIST IN DATA.JSON</p>";  
+                }  
+           }  
+           else  
+           {  
+                $error = 'JSON File not exits';  
+           }  
+      }  
+ }  
+ ?>  
+ <!DOCTYPE html>  
+ <html>  
+      <head>  
+           <title></title>  
+           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
+           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+      </head>  
+      <body>  
+           <br />  
+           <div class="container" style="width:500px;">  
+                <h3 align="">FORGOT PASSWORD</h3><br />                 
+                <form method="post">  
+                     <?php   
+                     if(isset($error))  
+                     {  
+                          echo $error;  
+                     }  
+                     ?>  
+                     <br />    
+                     <label>E-mail</label>
+                     <input type="text" name = "email" class="form-control" /><br />
+
+                     
+                     <input type="submit" name="submit" value="Submit" class="btn btn-info" />                        
+                     <?php  
+                     if(isset($message))  
+                     {  
+                          echo $message;  
+                     }  
+                     ?>  
+                </form>  
+           </div>  
+           <br />  
+      </body>  
+ </html>  
